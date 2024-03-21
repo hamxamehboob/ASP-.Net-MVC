9322f21292bcf44e5c66f34e622937eb1285971b
hamxamehboob <hamzamehboob2003@gmail.com>
2024-03-21 22:23:09 +0500

    Add project files.

---
+++ Hamza_MVC/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hamza_MVC.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Semester { get; set; }
+
+    public string? Department { get; set; }
+
+    public int? EarnedCreditHours { get; set; }
+}
